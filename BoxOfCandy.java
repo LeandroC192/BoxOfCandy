46053faa3e5f1cc84f44551463c67b6beb1d23e2
LeandroC192 <Leandroc16@nycstudents.net>
2025-02-03 16:48:49 +0000

    Worked on BoxOfCandy and Beginning test cases

--- BoxOfCandy.java
+++ BoxOfCandy.java
@@ -3,6 +3,24 @@ public class BoxOfCandy
     /** box contains at least one row and is initialized in the constructor. */
     private Candy[][] box;
 
+    public BoxOfCandy(Candy[][] b)
+    {
+        box = b;
+    }
+    
+    public String toString()
+    {
+        String s = "";
+        for(int row = 0; row < box.length; row++)
+        {
+            for(int col = 0; col < box[0].length; col++)
+            {
+                s += box[row][col]+" ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
     /**
     * Moves one piece of candy in column col, if necessary and possible, so that the box
     * element in row 0 of column col contains a piece of candy, as described in part (a).
@@ -11,7 +29,17 @@ public class BoxOfCandy
     */
     public boolean moveCandyToFirstRow(int col)
     { 
-        /* to be implemented in part (a) */ 
+        /* to be implemented in part (a) */
+        for(int i = 1; i < box.length; i++)
+        {
+            if(box[i][col] != null)
+            {
+                box[0][col] = box[i][col];
+                box[i][col] = null;
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -21,6 +49,19 @@ public class BoxOfCandy
     public Candy removeNextByFlavor(String flavor)
     { 
         /* to be implemented in part (b) */ 
+        for(int row = box.length - 1; row >= 0; row--)
+        {
+            for(int col = 0; col < box[0].length; col++)
+            {
+                Candy c = box[row][col];
+                if(c != null && c.getFlavor().equals(flavor))
+                {
+                    box[row][col] = null;
+                    return c;
+                }
+            }
+        }
+        return null;
     }
 
     // There may be instance variables, constructors, and methods that are not shown.
--- Main.java
+++ Main.java
@@ -3,6 +3,19 @@ public class Main
   public static void main (String [] args)
   {
     Candy[][] box = new Candy[4][3];
+    box[0][1] = new Candy("Lime");
+    box[1][1] = new Candy("Orange");
+    box[2][2] = new Candy("Cherry");
+    box[3][1] = new Candy("Lemon");
+    box[3][2] = new Candy("Grape");
+    System.out.println(box);
+
+    BoxOfCandy b = new BoxOfCandy(box);
+    System.out.println(b);
+    b.moveCandyToFirstRow(2);
+    System.out.println(b);
+
+    box = new Candy[3][5];
     
-  }
+    }
 }
